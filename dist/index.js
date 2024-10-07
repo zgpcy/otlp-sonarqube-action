c5af4b4c7a24c4ad0eeb858c5ee456a5b4f96993
Zacharias Georgiou <zacharias.georgiou@jinius.com.cy>
2024-10-07 16:03:03 +0300

    wip

--- dist/index.js
+++ dist/index.js
@@ -59520,11 +59520,35 @@ exports.sendMetrics = sendMetrics;
 /***/ }),
 
 /***/ 7069:
-/***/ ((__unused_webpack_module, exports) => {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__nccwpck_require__(2186));
 class SonarQube {
     requestHeaders = new Headers();
     url;
@@ -59603,8 +59627,10 @@ class SonarQube {
     getSecurityHotspotsMetrics = async () => {
         const otlpMetrics = [];
         const projects = await this.getAllProjects(this.prefixes);
+        core.debug(`Found ${projects.length} projects`);
         for (const project of projects) {
             const hotspots = await this.getSecurityHotspots(project.key, project.owner, project.repository);
+            core.debug(`Found ${hotspots.length} hotspots for ${project.key}`);
             for (const hotspot of hotspots) {
                 const otlpMetric = {
                     name: 'hotspot.detected',
